8aa30e322d985687ada90bf5cbf61c5e882d2227
kabirsadman <sadmankabirsami9@gmail.com>
2022-01-03 01:31:50 +0600

    using_foreach_loop

---
+++ Array/Array/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] cars = { "lol", "rofl", "tol" };
+            //for(int i=0; i<cars.Length; i++)
+            //{
+                //Console.WriteLine(cars[i]);
+            //}
+            //another way of declare array
+            // String[] cars= new String[3];
+            // cars[0]="lol";
+            //cars[1]="rofl";
+            //cars[2]="tofl";
+
+            //printing the value of array using foreach loop
+
+            foreach(String car in cars)
+            {
+                Console.WriteLine(car);
+
+            }    
+
+
+
+
+
+            Console.ReadLine(); 
+        }
+    }
+}
