9f0609e374692a7d6d96b0799330b172490e88d7
kabirsadman <sadmankabirsami9@gmail.com>
2022-01-03 16:46:18 +0600

    invoking_method_in_main_method

---
+++ method/method/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String name = "Kabir";
+            Myself(name);
+            Myself(name);
+            Console.ReadKey();
+            
+
+          
+        }
+        
+        static void Myself(String name)
+        {
+            Console.WriteLine("I am "+name);
+            Console.WriteLine("I love C#");
+            Console.WriteLine("I did this course a while ago now I forget the basic");
+            Console.WriteLine("So I am brainstorming again");
+        }
+        
+    }
+   
+   
+}
