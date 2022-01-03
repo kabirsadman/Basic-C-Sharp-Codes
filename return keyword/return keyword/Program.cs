f5e71ee7592d8731a8edb5e5b159b2833d96711f
kabirsadman <sadmankabirsami9@gmail.com>
2022-01-04 00:30:26 +0600

    invoking_method_with_return_keyword

---
+++ return keyword/return keyword/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace return_keyword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            Console.WriteLine("Enter the value of x:");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the value of y:");
+            double y = Convert.ToDouble(Console.ReadLine());
+            double result = Multipy(x,y);
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+
+        }
+        static double Multipy(double x, double y)
+        {
+            double z = x * y;
+            return z;
+
+        }
+
+       
+    }
+}
