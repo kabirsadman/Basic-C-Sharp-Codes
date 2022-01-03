9dc7f9892caf80f449289a540d006b592d7c9b6e
kabirsadman <sadmankabirsami9@gmail.com>
2022-01-04 02:18:29 +0600

    params_keyword_for_dealing_with_method_easily

---
+++ params keyword/params keyword/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace params_keyword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = CheckOut(5,4,3);
+            Console.WriteLine(result);
+            Console.ReadKey();
+
+        }
+        static double CheckOut(params double[] prices)
+        {
+            double total = 0;
+            foreach(double price in prices)
+            {
+                total += price;
+            }
+            return total;
+
+        }
+    }
+}
