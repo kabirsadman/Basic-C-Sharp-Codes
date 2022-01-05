c329ea29bf72409e42cb2ad1a343e5b88fe7f2ce
kabirsadman <sadmankabirsami9@gmail.com>
2022-01-06 01:08:54 +0600

    class

---
+++ Class/Class/Messages.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    static class Messages
+    {
+        public static void Hello()
+        {
+            Console.WriteLine("Hello from another class");
+        }
+        public static void Myself()
+        {
+            Console.WriteLine("Myself Sami");
+        }
+        public static void Bye()
+        {
+            Console.WriteLine("Ok now bye!");
+        }
+    }
+}
