206f2ba7534c16877583e9062d657d87be4962ad
kabirsadman <sadmankabirsami9@gmail.com>
2022-01-06 01:23:00 +0600

    invoking_another_class_in_main_class_by_creatig_object

---
+++ object/object/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @object
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Human human = new Human();
+            human.name = "Sami";
+            human.age = 24;
+            human.Sleep();
+            human.Eat();
+            Console.ReadKey();
+
+        }
+    }
+    class Human
+    {
+        public String name; //field
+        public int age; //field
+        public void Eat()
+        {
+            Console.WriteLine(name+ " "+ "is eating");
+        }
+        public void Sleep()
+        {
+            Console.WriteLine(name+ " "+ "is sleeping" );
+        }
+
+
+    }
+
+}
