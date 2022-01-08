74fcab2e48940374c28c16c09fd1df7cf449c079
kabirsadman <sadmankabirsami9@gmail.com>
2022-01-09 03:33:32 +0600

    array_of_object

---
+++ Array Of Object/Array Of Object/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Of_Object
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car[] garage = { new Car("BMW"), new Car("Corolla"), new Car("Toyota") };
+            foreach(Car car in garage)
+            {
+                Console.WriteLine(car.model);
+            }
+            Console.ReadKey();
+
+        }
+    }
+    class Car
+    {
+        public string model;
+        public Car(String model)
+        {
+            this.model = model;
+        }
+    }
+}
