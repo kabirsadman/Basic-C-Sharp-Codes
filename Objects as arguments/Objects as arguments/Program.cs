a33fef99807f981f1d511f0e115cc0fdf9e60f17
kabirsadman <sadmankabirsami9@gmail.com>
2022-01-10 02:53:49 +0600

    Complex_One_Review_It

---
+++ Objects as arguments/Objects as arguments/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objects_as_arguments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car("Mustang", "red");
+            ChangeColor(car1, "green");
+            Console.WriteLine(car1.color+ " "+ car1.model);
+            Console.ReadKey();
+
+        }
+        public static void ChangeColor(Car car, String color)
+        {
+            car.color = color;
+            
+        }
+    }
+    class Car
+    {
+        public String model;
+        public String color;
+        public Car(String model,String color)
+        {
+            this.model = model;
+            this.color = color;
+        }
+    }
+}
