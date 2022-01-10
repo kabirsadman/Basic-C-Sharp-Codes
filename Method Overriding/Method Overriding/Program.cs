bed6cb20ec93bda545b0c775fc9369d2a3a80d8f
kabirsadman <sadmankabirsami9@gmail.com>
2022-01-10 16:08:01 +0600

    method_overriding

---
+++ Method Overriding/Method Overriding/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Overriding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            Cat cat =new Cat();
+            cat.Speak();
+            dog.Speak();
+            Console.ReadKey();
+        }
+    }
+    class Animal
+    {
+        public virtual void Speak()
+        {
+            Console.WriteLine("The animal can speak!!");
+        }
+    }
+    class Dog : Animal
+    {
+        public override void Speak()
+        {
+            Console.WriteLine("The animal can jump");
+        }
+
+    }
+    class Cat: Animal
+    {
+
+    }
+    
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Basic-C-Sharp-Codes
+# I am uploading these codes just to memorize the basics of C#
+# I think when I'll be working on big projects(again) future, I'll be able to take help from my own made repository
